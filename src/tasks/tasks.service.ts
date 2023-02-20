6b248e0de4a697ac827a0dce2b072edc0bf37d74
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-02-20 13:51:04 +0500

    Get Task By Specific ID

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,6 +13,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
 
