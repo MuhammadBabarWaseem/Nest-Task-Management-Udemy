8304a979b60499b70a773f786d893770d227912c
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-02-22 01:59:49 +0500

    Done Error Handling Of; Username Exists

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
@@ -13,6 +17,15 @@ export class AuthService {
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.usersRepository.create({ username, password });
-    await this.usersRepository.save(user);
+
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username Already Exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
