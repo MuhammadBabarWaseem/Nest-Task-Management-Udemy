a40fe57ea511d412ce2811ddba14253daff9a2a9
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-03-03 20:57:53 +0500

    improvement

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
